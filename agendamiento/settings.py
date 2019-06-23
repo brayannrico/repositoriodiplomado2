a4ea1574b0a69ab4540a51b5d93c2e1b2111367f
brayan rico <brayanestremo14@gmail.com>
2019-06-23 12:19:40 -0500

    cambisos setings

--- agendamiento/settings.py
+++ agendamiento/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    
+
 ]
 
 ROOT_URLCONF = 'agendamiento.urls'
